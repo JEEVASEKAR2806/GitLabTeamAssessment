0fa8f7c5c81ab6d66821949abef0599280e1956d
Jothilingam Ashokkumar <jothilingam.ashokkumar@relevantz.com>
2024-10-04 17:48:56 +0530

    jothi

--- demo.java
+++ demo.java
@@ -5,6 +5,10 @@ class Demo{
 	publi int sub(int a , int b){
 		 return a-b;
 	}
+	  publi int mul(int a , int b){
+                 return a*b;
+        }
+
 	public static void main(String args[]){
 		System.out.println(addition(5,5));
 		System.out.println(sub(4,2));
